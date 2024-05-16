6fdcc420a9693024ccc2c5d8171137a1625640bd
Gian Ira B. Lariosa <gianlariosa@gmail.com>
2024-05-16 22:28:49 +0800

    Added priest to 1st lane

--- src/buttongrid.js
+++ src/buttongrid.js
@@ -167,7 +167,8 @@ export default function ButtonGrid({currentTime, user_id}) {
         <Block position={position} setPosition={setPosition} animationIntervalTime={animationIntervalTime} initialXRange={{ min: 0, max: 800 }} initialY={140} speed={speed} time={currentTime} count = {counter} user_id  = {user_id}/>
         <Block position={position} setPosition={setPosition} animationIntervalTime={animationIntervalTime} initialXRange={{ min: 0, max: 800 }} initialY={565} speed={speed} time={currentTime} count = {counter} user_id  = {user_id}/>
         <Block position={position} setPosition={setPosition} animationIntervalTime={animationIntervalTime} initialXRange={{ min: 0, max: 800 }} initialY={425} speed={speed} time={currentTime} count = {counter} user_id  = {user_id}/>
-        <Block position={position} setPosition={setPosition} animationIntervalTime={animationIntervalTime} initialXRange={{ min: 0, max: 800 }} initialY={280} speed={speed} time={currentTime} count = {counter} user_id  = {user_id}/> 
+        <Block position={position} setPosition={setPosition} animationIntervalTime={animationIntervalTime} initialXRange={{ min: 0, max: 800 }} initialY={280} speed={speed} time={currentTime} count = {counter} user_id  = {user_id}/>
+        <Block position={position} setPosition={setPosition} animationIntervalTime={animationIntervalTime} initialXRange={{ min: 0, max: 0 }} initialY={635} speed={speed} time={currentTime} count = {counter} user_id  = {user_id}/>
         <img
           src={currentImageSet[currentImageIndex]}
           alt="Animated Image"
